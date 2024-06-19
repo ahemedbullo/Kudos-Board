2e41a8b30276470653bdce317677ac0be385cd1e
ahemedbullo <ahemedbullo@gmail.com>
2024-06-19 15:37:35 -0700

    fixed error

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express()
+
+require("dotenv").config();
+const cors = require("cors");
+
+const PORT = 3000
+
+app.use(express.json())
+
+
+const boards = require("./routes/routes.js");
+
+app.use(cors());
+app.use(express.json());
+app.use("/boards", boards);
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my App")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+
+
+
+
